a16e4a8af039d47b2c1fd25b225b94bcffbd3adb
Mada Gower <gmgower@gmail.com>
2019-12-18 22:15:36 -0600

    Setup auth-router.js.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
 const bcrypt = require('bcryptjs');
-const jwt =require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const Users = require('./auth-model.js');
+
+const secrets = require('../config/secrets.js');
 
 router.post('/register', (req, res) => {
     let user = req.body;
@@ -22,7 +24,37 @@ router.post('/register', (req, res) => {
 router.post('login', (req, res) => {
     let {username, password} = req.body;
     
-    Users.findBy({})
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            const token = getJwtToken(user);
+    
+            res.status(200).json({
+              message: `Welcome ${user.username}, have a token.`,
+              token
+            });
+          } else {
+            res.status(401).json({ message: 'Invalid Credentials' });
+          }
+    })
+    .catch(error => {
+        res.status(500).json(error);
+      });
 })
+
+function getJwtToken(user) {
+    const payload = {
+      subject: user.id,
+      username: user.username, 
+    };
+  
+    const options = {
+      expiresIn: '1hr'
+    };
+  
+    //? s16 secret.jwtSecret
+    return jwt.sign(payload, secrets.jwtSecret, options)
+  }
 
 module.exports = router;
