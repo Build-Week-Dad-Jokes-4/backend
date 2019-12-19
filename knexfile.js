02284435dc0a8a2306ee0106074f234d1fb7c0d8
Mada Gower <gmgower@gmail.com>
2019-12-19 15:15:32 -0600

    Deploy Test9.

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,14 @@
 // Update with your config settings.
 require("dotenv").config()
 
+localPbConnection = {
+  host: 'localhost',
+  database: 'Database',
+  user: process.env.DB_USERS,
+  password: process.env.DB_PASS
+}
+const prodDbConnection = process.env.DATABASE_URL || localPbConnection
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -23,7 +31,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: prodDbConnection,
     pool: {
       min: 2,
       max: 10
